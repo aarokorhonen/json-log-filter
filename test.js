93f2e5e7c55f193a4212333abc5e246bc50a7e6a
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2020-11-15 20:34:16 +0200

    Boostrap a test module

---
+++ test.js
@@ -0,0 +1,34 @@
+const process = require("process");
+const childProcess = require("child_process");
+
+const indexModulePath = "index.js";
+const level = "30";
+
+const p = childProcess.spawn(process.argv[0], [indexModulePath, level]);
+
+const expected = `{"level":40}
+{"level":40}
+{"level":30}
+`;
+
+let t = "";
+
+p.stdout.on("data", (data) => {
+    console.log(String(data));
+    t += String(data);
+});
+
+p.stdout.on("end", (data) => {
+    const result = t === expected;
+    const exitCode = result ? 0 : 1;
+    process.exitCode = exitCode;
+});
+
+p.stderr.on("data", (data) => {
+    console.error(String(data));
+});
+
+p.stdin.write('{ "level": 40 }\n');
+p.stdin.write('{ "level": 40 }\n');
+p.stdin.write('{ "level": 30 }\n');
+p.stdin.end('{ "level": 20 }\n');
