119170224c8a1210446998369021354ec342fa49
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2020-11-22 15:12:14 +0200

    Add more tests to args.test

--- src/args.test.ts
+++ src/args.test.ts
@@ -13,6 +13,34 @@ describe("Arguments parsing", () => {
             output += String(data);
         });
 
+        proc.stderr.on("data", (error) => {
+            console.error(String(error));
+            done(error);
+        });
+
+        proc.on("exit", (exitCode) => {
+            expect(output).toMatch(/Usage:.*/);
+            expect(exitCode).toBe(0);
+            done();
+        });
+    });
+
+    test("Correctly identifies --help even when it's not the first arg", (done) => {
+        assertDoneFn(done);
+
+        const proc = spawnIndexModule("20 --help");
+
+        let output = "";
+
+        proc.stdout.on("data", (data) => {
+            output += String(data);
+        });
+
+        proc.stderr.on("data", (error) => {
+            console.error(String(error));
+            done(error);
+        });
+
         proc.on("exit", (exitCode) => {
             expect(output).toMatch(/Usage:.*/);
             expect(exitCode).toBe(0);
