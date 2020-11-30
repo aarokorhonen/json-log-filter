cdfa8ac2b227852abf575d6dc4acdc315fd58ecc
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2020-11-30 22:41:25 +0200

    Improve error handling for invalid JSON

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,20 @@ const rl = readline.createInterface({
 const config = parseConfigFromArgs(process.argv.slice(2));
 
 rl.on("line", (line) => {
-    const entry = JSON.parse(line);
-    if (
-        typeof config.minLogLevel === "number" &&
-        typeof entry.level === "number" &&
-        entry.level < config.minLogLevel
-    ) {
-        return;
-    } else {
-        const output = JSON.stringify(entry);
-        process.stdout.write(`${output}\n`);
+    try {
+        const entry = JSON.parse(line);
+        if (
+            typeof config.minLogLevel === "number" &&
+            typeof entry.level === "number" &&
+            entry.level < config.minLogLevel
+        ) {
+            return;
+        } else {
+            const output = JSON.stringify(entry);
+            process.stdout.write(`${output}\n`);
+        }
+    } catch (err) {
+        console.error(`Invalid JSON line: "${line}" (${err})`);
+        process.exit(1);
     }
 });
